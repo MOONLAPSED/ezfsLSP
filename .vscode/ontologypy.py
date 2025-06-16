84c88d8fa716045b6935812a294e346d9decbc84
MOONLAPSED <MOONLAPSED@gmail.com>
2025-06-16 13:05:52 -0500

    iwe typescript+python LSP for vscode

--- .vscode/ontologypy.py
+++ .vscode/ontologypy.py
@@ -1,4 +1,7 @@
 from __future__ import annotations
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# LICENSE © 2025: CC BY 4.0: MOONLAPSED:https://github.com/Moonlapsed/Morphological
 import hashlib
 import math
 import hashlib
